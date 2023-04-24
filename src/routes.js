1ec07b49e2f6744360b4ccf1bfaedff8d3a16697
Gustavo Bressan <67989020+gussXX@users.noreply.github.com>
2023-04-24 15:19:31 -0300

    cors

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router();
 
 const {first} = require('./routes/first.js');
-const { two } = require('./routes/two.js');
+const {two} = require('./routes/two.js');
+const {cors} = require ('./routes/cors.js')
 
 router.post('/first', first)
 router.get('/two', two)
+router.get('/cors', cors)
 
 module.exports = router;
