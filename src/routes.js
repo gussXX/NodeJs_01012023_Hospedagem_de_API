7f92d7ec1df57e8f3cd50452771d07acfdeb5ce2
Gustavo Bressan <67989020+gussXX@users.noreply.github.com>
2023-05-02 11:00:48 -0300

    cors

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {first} = require('./routes/first.js');
 const {two} = require('./routes/two.js');
+var cors = require('cors')
+// const cors = require('amp-toolbox-cors/lib/cors.js');
 // const {cors} = require ('./routes/cors.js')
 
 router.post('/first', first)
-router.get('/two', two)
+router.get('/two', cors(), two)
 // router.get('/cors',)
 
 module.exports = router;
