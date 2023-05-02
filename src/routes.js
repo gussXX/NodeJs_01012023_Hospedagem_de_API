65bda2e55fed595372e317d0baa1d45d0d8a486c
Gustavo Bressan <67989020+gussXX@users.noreply.github.com>
2023-05-02 11:03:52 -0300

    cors

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,18 @@ const router = express.Router();
 const {first} = require('./routes/first.js');
 const {two} = require('./routes/two.js');
 var cors = require('cors')
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
 // const cors = require('amp-toolbox-cors/lib/cors.js');
 // const {cors} = require ('./routes/cors.js')
 
 router.post('/first', first)
-router.get('/two', cors(), two)
+router.get('/two', cors(corsOptions), two)
+
 // router.get('/cors',)
 
 module.exports = router;
