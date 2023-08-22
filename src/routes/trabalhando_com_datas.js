e4285b32ebf99fbb83d67514a3761ba3380d3666
Gustavo Bressan <67989020+gussXX@users.noreply.github.com>
2023-08-21 21:37:22 -0300

    Trabalhandp com datas

--- src/routes/trabalhando_com_datas.js
+++ src/routes/trabalhando_com_datas.js
@@ -1,18 +1,22 @@
 async function trabalhando_com_datas(req, res) {
     const requisition = req.body;
 
-    // Consulta a hora atual em UTC
-    const currentDateUTC = new Date();
+    const date = Date.now();
+    const currentdate = new Date(date);
 
-    // Define o fuso horário desejado (por exemplo, "America/Sao_Paulo")
-    const desiredTimezone = "America/Argentina/Buenos_Aires"; // Substitua pelo fuso horário desejado
+    const fuso = currentdate.getTimezoneOffset();
+    const timeFuso = date - fuso;
 
-    // Obtém o horário atual no fuso horário desejado
-    const currentTimezoneDate = new Date(currentDateUTC.toLocaleString('en-US', { timeZone: desiredTimezone }));
+    const atualTimeFuso = new Date(timeFuso);
 
-    console.log(currentTimezoneDate); // Saída como objeto Date ajustado
+    console.log('DATA ATUAL : ' + date)
+    console.log('DATA FUSO : ' + fuso)
+    console.log('DATA SUBTRAINDO : ' + timeFuso)
+    console.log('=============================')
+    console.log('HORA CONVERTIDA : ' + atualTimeFuso)
+    console.log('HORA ISOSTRING : ' + atualTimeFuso.toISOString())
 
-    res.status(200).json();
+    res.status(200).json(timeFuso);
 }
 
 module.exports = { trabalhando_com_datas };
